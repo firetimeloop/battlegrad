cf4d9e3692d7db1cfad821008a7c4832a469b7b1
Kirill Vorfolomeev <cyrill.vorfolomeev@yandex.ru>
2023-04-19 21:28:23 +0500

    PRA-11 Инициализация проекта, установка пакетов, добавление линтеров для кода и стилей

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,69 @@ module.exports = {
     browser: true,
     es2020: true,
     node: true,
+    jest: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'airbnb',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks',
+  ],
   rules: {
+    'max-len': [2, 100],
     '@typescript-eslint/ban-ts-comment': 1,
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'no-undef': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true
+      }
+    ],
+    'no-underscore-dangle':  [
+      'error',
+      { 
+        allow: ["__SERVER_PORT__"] 
+      }
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { 
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx'
+        ]
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+   ]
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js','.ts', '.tsx']
+      }
+    }
+},
 }
