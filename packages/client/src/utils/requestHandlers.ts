309a8ec6fe33c25c8e7e91af51c415cad8d11986
german <sorokin@deck.lc>
2023-05-11 12:58:25 +0300

    PRA-14 fix after review

--- packages/client/src/utils/requestHandlers.ts
+++ packages/client/src/utils/requestHandlers.ts
@@ -2,20 +2,25 @@ import { AxiosError, AxiosResponse } from 'axios';
 import { store } from '../app/store';
 import { setAlert } from '../components/Alert/slice';
 
-const skipAlert = [
+const skipRequests = [
   { url: '/auth/user', method: 'get' },
 ];
 
 export const errorHandler = (error: AxiosError<{reason?: string}>) => {
-  // сюда можно добавить действия для всех запросов с ошибкой
-  console.log('Handled error', error);
-  if (
-    !skipAlert.some((item) => (
-      item.url === error.config?.url
-      && item.method === error.config.method?.toLowerCase()
-    ))
-    && error.response?.data?.reason
-  ) {
+  if (error.config === undefined) {
+    return Promise.reject(error);
+  }
+
+  const { url, method } = error.config;
+
+  // показываем алерт только для запросов которых нет в skipRequests
+  const notSkipped = !skipRequests.some((item) => {
+    const sameUrl = item.url === url;
+    const sameMethod = item.method === method?.toLowerCase();
+    return sameUrl && sameMethod;
+  });
+
+  if (notSkipped && error.response?.data?.reason) {
     store.dispatch(setAlert(error.response?.data?.reason));
   }
 
