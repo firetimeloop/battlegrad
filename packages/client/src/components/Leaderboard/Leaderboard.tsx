5e2f58294fb801fb997d80cca72d4d3cc7f02053
Elvg Ovadykov <oelvg@mail.ru>
2023-06-10 18:54:20 +0300

    Исправление ошибок lint'a

--- packages/client/src/components/Leaderboard/Leaderboard.tsx
+++ packages/client/src/components/Leaderboard/Leaderboard.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useTheme } from 'styled-components';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import LeaderboardTable from './components/LeaderboardTable/LeaderboardTable';
+import LeaderboardTable from './components/LeaderboardTable';
 import { GetLeadersByTeamName } from './slice';
 import Loader from '../Loader';
 import { LoaderSizeEnum } from '../../enum';
