8aeb4ecb88852f749a3ad484348db6c4b5b8c245
SerghAl <sergey.alabuzhev.personal@gmail.com>
2023-07-16 15:38:12 +0400

    add csp, clean console output and add key to lists

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -5,6 +5,7 @@ import { LoaderContainer } from '@components/Loader/styles';
 import Loader from '@components/Loader';
 import { GetMe } from '@components/Auth/slice';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Layout from '../Layout';
 import logo from '../../../public/logo.png';
 import { AppWrapper } from './styles';
@@ -16,9 +17,6 @@ import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { LoaderSizeEnum } from '../../enum';
 import cspSettings from '../../app/csp';
 
-// Пока без SSR пропишу явно
-const __SERVER_PORT__ = 3001;
-
 function App() {
   const { user, needFetchUser } = useAppSelector((state) => state.auth);
 
@@ -27,6 +25,14 @@ function App() {
 
   const dispatch = useAppDispatch();
   useEffect(() => {
+    // Постоянно выдает ошибку при парсинге json, что понятно, так как возвращается html.
+    // Да и смысла в вызове функции не увидел
+    // const fetchServerData = async () => {
+    //   const url = `http://localhost:${__SERVER_PORT__}`;
+    //   const response = await fetch(url);
+    //   const data = await response.json();
+    //   console.log(data);
+    // };
     // Постоянно выдает ошибку при парсинге json, что понятно, так как возвращается html.
     // Да и смысла в вызове функции не увидел
     // const fetchServerData = async () => {
@@ -73,12 +79,40 @@ function App() {
               width="700px"
               height="auto"
               className="logo"
+    <HelmetProvider>
+      <ThemeProvider theme={selectedTheme}>
+        <Helmet>
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content={cspSettings}
+          />
+        </Helmet>
+        <AppWrapper>
+          {loggedIn ? (
+            <Layout setSelectedTheme={setSelectedTheme} />
+          ) : (
+            <img
+              src={logo}
+              alt=""
+              width="700px"
+              height="auto"
+              className="logo"
             />
+          )}
+          {needFetchUser ? (
           )}
           {needFetchUser ? (
             <LoaderContainer>
               <Loader size={LoaderSizeEnum.medium} />
             </LoaderContainer>
+          ) : (
+            <Router />
+          )}
+          <Alert />
+          <div />
+        </AppWrapper>
+      </ThemeProvider>
+    </HelmetProvider>
           ) : (
             <Router />
           )}
