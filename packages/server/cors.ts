849a0e2c573ce3328c3000a4957a6cbe0ca3a49e
Elvg Ovadykov <oelvg@mail.ru>
2023-07-16 16:59:52 +0300

    Fix cors

--- packages/server/cors.ts
+++ packages/server/cors.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 
-const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'http://62.84.121.9:3000'];
 
 const corsOptions = {
   origin: whitelist,
