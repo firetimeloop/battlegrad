bff8f297d3c46e4a25d1bb7d2d1be518ea557838
SerghAl <sergey.alabuzhev.personal@gmail.com>
2023-07-20 21:08:36 +0400

    Set static port

--- packages/client/src/app/csp.ts
+++ packages/client/src/app/csp.ts
@@ -4,10 +4,10 @@
 const cspSettings = `
                       default-src 'self';
                       base-uri 'self';
-                      connect-src 'self' localhost:${__SERVER_PORT__} localhost:24678 https://ya-praktikum.tech/api/v2/;
+                      connect-src 'self' localhost:3000 localhost:3001 localhost:24678 https://ya-praktikum.tech/api/v2/;
                       font-src 'self';
                       form-action 'self';
-                      img-src 'self' localhost:${__SERVER_PORT__} https://ya-praktikum.tech/api/v2/;
+                      img-src 'self' localhost:3000 localhost:3001 https://ya-praktikum.tech/api/v2/;
                       object-src 'self';
                       script-src-attr 'self';
                       style-src 'self' 'unsafe-inline';
