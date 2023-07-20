7c4c7fd613d768c0a4896c8655488707cc1a8a57
SerghAl <sergey.alabuzhev.personal@gmail.com>
2023-07-20 20:58:22 +0400

    Rebase

--- packages/client/src/components/App/App.tsx
+++ packages/client/src/components/App/App.tsx
@@ -5,7 +5,6 @@ import { LoaderContainer } from '@components/Loader/styles';
 import Loader from '@components/Loader';
 import { GetMe } from '@components/Auth/slice';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Layout from '../Layout';
 import logo from '../../../public/logo.png';
 import { AppWrapper } from './styles';
@@ -25,14 +24,6 @@ function App() {
 
   const dispatch = useAppDispatch();
   useEffect(() => {
-    // Постоянно выдает ошибку при парсинге json, что понятно, так как возвращается html.
-    // Да и смысла в вызове функции не увидел
-    // const fetchServerData = async () => {
-    //   const url = `http://localhost:${__SERVER_PORT__}`;
-    //   const response = await fetch(url);
-    //   const data = await response.json();
-    //   console.log(data);
-    // };
     // Постоянно выдает ошибку при парсинге json, что понятно, так как возвращается html.
     // Да и смысла в вызове функции не увидел
     // const fetchServerData = async () => {
@@ -78,41 +69,12 @@ function App() {
               alt=""
               width="700px"
               height="auto"
-              className="logo"
-    <HelmetProvider>
-      <ThemeProvider theme={selectedTheme}>
-        <Helmet>
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content={cspSettings}
-          />
-        </Helmet>
-        <AppWrapper>
-          {loggedIn ? (
-            <Layout setSelectedTheme={setSelectedTheme} />
-          ) : (
-            <img
-              src={logo}
-              alt=""
-              width="700px"
-              height="auto"
-              className="logo"
-            />
-          )}
-          {needFetchUser ? (
+              className="logo" />
           )}
           {needFetchUser ? (
             <LoaderContainer>
               <Loader size={LoaderSizeEnum.medium} />
             </LoaderContainer>
-          ) : (
-            <Router />
-          )}
-          <Alert />
-          <div />
-        </AppWrapper>
-      </ThemeProvider>
-    </HelmetProvider>
           ) : (
             <Router />
           )}
